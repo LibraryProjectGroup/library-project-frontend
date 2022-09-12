a0b1bea84305e93be99344b1b6bcd4abdb006129
jazmc <jasmin.lumme@gmail.com>
2022-09-12 12:06:12 +0300

    Update App.tsx test to match current situation #1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByText(/fetch book data/i);
+  expect(buttonElement).toBeInTheDocument();
 });
