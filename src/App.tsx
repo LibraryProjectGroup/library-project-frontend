83856e33fc45b540a7a7accf7690d7ba7ac997d8
Eeli Killström <eeli.killstrom@gmail.com>
2022-09-17 14:34:11 +0300

    Changed endpoint to fetch db data

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Book from './interfaces/book.interface';
 
@@ -6,8 +6,19 @@ function App() {
 
   const [exampleBook, setExampleBook] = useState<Book | undefined>(undefined)
   
+  useEffect(()=>{
+    console.log("exampleBook changed to:")
+    console.log(exampleBook)
+  },[exampleBook])
+
   function fetchExampleBookData() {
     fetch('http://localhost:3001/example', {headers:{'Access-Control-Allow-Origin':"http://localhost:3000/example"}})
+      .then(response => response.json())
+      .then(data => setExampleBook(data));
+  }
+
+  function fetchBookFromDb(bookId:string){
+    fetch(`http://localhost:3001/book?id=${bookId}`, {headers:{'Access-Control-Allow-Origin':"http://localhost:3000/book"}})
       .then(response => response.json())
       .then(data => setExampleBook(data));
   }
@@ -15,7 +26,7 @@ function App() {
   function renderExampleBookData(){
     return(
       <div style={{border: "solid saddlebrown 2px"}}>
-        <p>Owner: {exampleBook?.owner}</p>
+        <p>Loaned to: {exampleBook?.owner}</p>
         <p>Title: {exampleBook?.title}</p>
         <p>Author: {exampleBook?.author}</p>
         <p>Topic: {exampleBook?.topic}</p>
@@ -28,7 +39,7 @@ function App() {
   return (
     <div className="App">
       <button 
-        onClick={()=>{fetchExampleBookData()}} 
+        onClick={()=>{fetchBookFromDb("1")}} 
         style={{width: 200, height: 100, backgroundColor: "yellow", border: "solid black 2px", borderRadius: "2px"}}
       >
         Fetch book data
