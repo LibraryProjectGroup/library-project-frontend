42b46f475df9c690ad5e6bc8536a5607cdf18d74
el1sa <elisa.tuovila@myy.haaga-helia.fi>
2022-09-29 10:45:16 +0300

    folder for test cases #4

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 // initial test to check if add button is present:
 test('renders add button', () => {
