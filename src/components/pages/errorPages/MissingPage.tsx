c1fdb38d7093f687a2d2feb981ed513767e11951
pisanty <pisantyshay@gmail.com>
2022-11-09 19:47:15 +0200

    third commit
    attempt to fix the Syntax error: JSX expressions must have one parent element

--- src/components/pages/errorPages/MissingPage.tsx
+++ src/components/pages/errorPages/MissingPage.tsx
@@ -1,21 +1,19 @@
 import { FC } from "react";
 import { Button } from "@mui/material";
 import { userPageReturnButton } from "../../../sxStyles";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const Missing: FC = () => {
     return (
-     <h1 style="text-align:center">404: NOT FOUND</h1>
-     <h3 style="text-align:center">Sorry, the page doesn't exist</h3>
-        <Button
+     <><h1 style="text-align:center">404: NOT FOUND</h1><h3 style="text-align:center">Sorry, the page doesn't exist</h3><Button
             sx={userPageReturnButton}
             variant="contained"
             onClick={() => {
-                navigate("/list-books");
-            }}
+                Navigate("/list-books");
+            } }
         >
             Return
-        </Button>
+        </Button></>
         
     );
 
--- src/components/pages/errorPages/UnauthorizedPage.tsx
+++ src/components/pages/errorPages/UnauthorizedPage.tsx
@@ -1,21 +1,19 @@
 import { FC } from "react";
 import { Button } from "@mui/material";
 import { userPageReturnButton } from "../../../sxStyles";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const Unauthorized: FC = () => {
     return (
-     <h1 style="text-align:center">Unauthorized</h1>
-     <h3 style="text-align:center">Sorry, access denied</h3>
-        <Button
+     <><h1 style="text-align:center">Unauthorized</h1><h3 style="text-align:center">Sorry, access denied</h3><Button
             sx={userPageReturnButton}
             variant="contained"
             onClick={() => {
-                navigate("/list-books");
-            }}
+                Navigate("/list-books");
+            } }
         >
             Return
-        </Button>
+        </Button></>
         
     );
 
