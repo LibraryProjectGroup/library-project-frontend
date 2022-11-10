3317218d141e7eac4439910cf5db09fc845fb3ea
Dung <jum.pham@hotmail.com>
2022-11-10 18:11:40 +0200

    added margin to button

--- src/sxStyles.ts
+++ src/sxStyles.ts
@@ -226,6 +226,7 @@ export const listBooksEntryAddButton: SxProps = {
     fontFamily: "Montserrat",
     fontWeight: "bold",
     fontSize: 15,
+    margin:"3px",
     //marginTop: "1rem",
     //width: "30%",
     whiteSpace: "nowrap",
