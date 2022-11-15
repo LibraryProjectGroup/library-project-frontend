85be582de7520ab1cadd8e0a01b6f3ed4f48b1b0
jongpants <jong.lau99@gmail.com>
2022-11-15 18:23:18 +0200

    removing leftover import

--- src/components/pages/userPage/UserPage.tsx
+++ src/components/pages/userPage/UserPage.tsx
@@ -20,7 +20,6 @@ import Borrow from "../../../interfaces/borrow.interface";
 import Snackbar from "@mui/material/Snackbar";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import { addSyntheticLeadingComment } from "typescript";
 
 const MyAccount: FC = (): JSX.Element => {
     const [books, setBooks] = useState<{ [key: number]: Book }>([]);
