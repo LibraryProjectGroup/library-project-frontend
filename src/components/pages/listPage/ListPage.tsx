a4bbaaae53fd150d0b9eb79db0033d8d902a559d
Dung <jum.pham@hotmail.com>
2022-11-28 11:28:04 +0200

    Revert "return-button route changed"
    This reverts commit 3d4fdefbd8eba66b580f65921f0729e3c8832362.

--- src/components/pages/listPage/ListPage.tsx
+++ src/components/pages/listPage/ListPage.tsx
@@ -45,7 +45,7 @@ const ListPage: FC = (): JSX.Element => {
             <Fab
                 aria-label="back"
                 sx={userPageBackButton}
-                onClick={() => navigate("/booklists")}
+                onClick={() => navigate("/")}
             >
                 <ArrowBackIcon />
             </Fab>
