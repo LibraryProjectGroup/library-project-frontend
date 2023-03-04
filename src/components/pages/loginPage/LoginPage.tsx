92bcd3c35ac9c7cb7b593ef55456585738be21ab
Amaru Korpimäki <amarukorpimaki@Amarus-MacBook-Pro.local>
2023-03-04 14:35:33 +0200

    testing runner

--- src/components/pages/loginPage/LoginPage.tsx
+++ src/components/pages/loginPage/LoginPage.tsx
@@ -3,168 +3,168 @@ import { Box, Typography, TextField, Button, Paper, Grid } from "@mui/material";
 import BACKEND_URL from "../../../backendUrl";
 import { useNavigate } from "react-router-dom";
 import {
-  loginButton,
-  loginBox,
-  AuthBoxTitle,
-  loginPaper,
-  textButton,
-  loginRegisterTitle,
-  loginRegisterContent,
+	loginButton,
+	loginBox,
+	AuthBoxTitle,
+	loginPaper,
+	textButton,
+	loginRegisterTitle,
+	loginRegisterContent,
 } from "../../../sxStyles";
 import { setSession } from "../../../auth";
 import { TheContext } from "../../../TheContext";
 
 const LoginPage: FC = (): JSX.Element => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMesssage] = useState("");
-  const navigate = useNavigate();
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [errorMessage, setErrorMesssage] = useState("");
+	const navigate = useNavigate();
 
-  const context = useContext(TheContext);
+	const context = useContext(TheContext);
 
-  const [dimensions, setDimensions] = React.useState({
-    height: window.innerHeight,
-    width: window.innerWidth,
-  });
+	const [dimensions, setDimensions] = React.useState({
+		height: window.innerHeight,
+		width: window.innerWidth,
+	});
 
-  useEffect(() => {
-    const handleResize = () => {
-      setDimensions({
-        height: window.innerHeight,
-        width: window.innerWidth,
-      });
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			setDimensions({
+				height: window.innerHeight,
+				width: window.innerWidth,
+			});
+		};
 
-    window.addEventListener("resize", handleResize);
+		window.addEventListener("resize", handleResize);
 
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
-  const handleLogin = async () => {
-    try {
-      const response = await fetch(`${BACKEND_URL}/auth/login`, {
-        method: "POST",
-        headers: {
-          "content-type": "application/json;charset=UTF-8",
-        },
-        body: JSON.stringify({ email: email, password: password }),
-      });
-      let data = await response.json();
-      if (data.ok) {
-        setSession(data.secret);
-        navigate("/list-books");
-        // update user data when you logIn and logOut
-        context?.setIsLogin(true);
-      } else {
-        setErrorMesssage(data.message ? data.message : "internal error");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+	const handleLogin = async () => {
+		try {
+			const response = await fetch(`${BACKEND_URL}/auth/login`, {
+				method: "POST",
+				headers: {
+					"content-type": "application/json;charset=UTF-8",
+				},
+				body: JSON.stringify({ email: email, password: password }),
+			});
+			let data = await response.json();
+			if (data.ok) {
+				setSession(data.secret);
+				navigate("/list-books");
+				// update user data when you logIn and logOut
+				context?.setIsLogin(true);
+			} else {
+				setErrorMesssage(data.message ? data.message : "internal error");
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  return (
-    <Grid
-      container
-      flex-wrap="wrap"
-      direction="row"
-      justifyContent="space-around"
-      alignItems="center"
-      sx={{
-        maxWidth: dimensions.width,
-        minHeight: dimensions.height,
-      }}
-    >
-      <Grid
-        item
-        container
-        alignItems="center"
-        sx={{ width: "95%", paddingBottom: 10 }}
-      >
-        <Grid item xs={12} md={7}>
-          <Box>
-            <Box sx={{ margin: "4rem 4rem 2rem 4rem" }}>
-              <Typography
-                variant="h1" //not responsive font
-                sx={loginRegisterTitle}
-              >
-                Efilibrary
-              </Typography>
-              <Typography sx={loginRegisterContent}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </Typography>
-            </Box>
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={5}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <Paper elevation={10} sx={loginPaper}>
-            <Box sx={{ padding: "3rem" }}>
-              <Typography variant="h4" sx={AuthBoxTitle}>
-                Login
-              </Typography>
-              <Box sx={loginBox}>
-                <TextField
-                  label="Email"
-                  variant="outlined"
-                  margin="normal"
-                  onChange={(event) => {
-                    setEmail(event.target.value);
-                  }}
-                />
+	return (
+		<Grid
+			container
+			flex-wrap="wrap"
+			direction="row"
+			justifyContent="space-around"
+			alignItems="center"
+			sx={{
+				maxWidth: dimensions.width,
+				minHeight: dimensions.height,
+			}}
+		>
+			<Grid
+				item
+				container
+				alignItems="center"
+				sx={{ width: "95%", paddingBottom: 10 }}
+			>
+				<Grid item xs={12} md={7}>
+					<Box>
+						<Box sx={{ margin: "4rem 4rem 2rem 4rem" }}>
+							<Typography
+								variant="h1" //not responsive font
+								sx={loginRegisterTitle}
+							>
+								Efilibrary Staging
+							</Typography>
+							<Typography sx={loginRegisterContent}>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+								enim ad minim veniam, quis nostrud exercitation ullamco laboris
+								nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+								in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+								nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+								sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</Typography>
+						</Box>
+					</Box>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					md={5}
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+					}}
+				>
+					<Paper elevation={10} sx={loginPaper}>
+						<Box sx={{ padding: "3rem" }}>
+							<Typography variant="h4" sx={AuthBoxTitle}>
+								Login
+							</Typography>
+							<Box sx={loginBox}>
+								<TextField
+									label="Email"
+									variant="outlined"
+									margin="normal"
+									onChange={(event) => {
+										setEmail(event.target.value);
+									}}
+								/>
 
-                <TextField
-                  label="Password"
-                  variant="outlined"
-                  type="password"
-                  margin="normal"
-                  onChange={(event) => {
-                    setPassword(event.target.value);
-                  }}
-                />
-                {errorMessage && (
-                  <Typography sx={{ color: "red" }}>{errorMessage}</Typography>
-                )}
-              </Box>
-              <Box sx={{ textAlign: "center" }}>
-                <Button
-                  variant="contained"
-                  onClick={handleLogin}
-                  sx={loginButton}
-                >
-                  Log in
-                </Button>
-              </Box>
-              <Box sx={{ textAlign: "center" }}>
-                <Button
-                  variant="text"
-                  onClick={() => navigate("/create-account")}
-                  sx={textButton}
-                >
-                  Create account
-                </Button>
-              </Box>
-            </Box>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Grid>
-  );
+								<TextField
+									label="Password"
+									variant="outlined"
+									type="password"
+									margin="normal"
+									onChange={(event) => {
+										setPassword(event.target.value);
+									}}
+								/>
+								{errorMessage && (
+									<Typography sx={{ color: "red" }}>{errorMessage}</Typography>
+								)}
+							</Box>
+							<Box sx={{ textAlign: "center" }}>
+								<Button
+									variant="contained"
+									onClick={handleLogin}
+									sx={loginButton}
+								>
+									Log in
+								</Button>
+							</Box>
+							<Box sx={{ textAlign: "center" }}>
+								<Button
+									variant="text"
+									onClick={() => navigate("/create-account")}
+									sx={textButton}
+								>
+									Create account
+								</Button>
+							</Box>
+						</Box>
+					</Paper>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default LoginPage;
