0de7b9d6981b34aa3440d05f6884d769bcc63e89
Julia Söderlund <julia.soderlund@yandex.ru>
2023-03-30 18:47:58 +0300

    Edit description on the login page

--- src/components/pages/loginPage/LoginPage.tsx
+++ src/components/pages/loginPage/LoginPage.tsx
@@ -114,14 +114,9 @@ const LoginPage: FC = (): JSX.Element => {
                   EfiLibrary
                 </Typography>
                 <Typography sx={loginRegisterContent}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  Welcome to EfiLibrary - the digital extension of our physical
+                  book collection. Borrow books, make reservations, create lists
+                  of favorites and immerse yourself in the world of knowledge.
                 </Typography>
               </Box>
             </Box>
