530699992e3a22fe0a419131204501fc8f39c52e
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-10-24 21:07:55 +0300

    even more styling updates to BookCard.tsx

--- src/components/pages/listBooksPage/BookCard.tsx
+++ src/components/pages/listBooksPage/BookCard.tsx
@@ -46,7 +46,7 @@ const BookCard: React.FC<BookCardProps> = ({
               display: 'flex',
               flexDirection: 'column',
               width: 'auto',
-              height: 650,
+              height: 650
             }
       }
       key={book.id}
@@ -175,7 +175,7 @@ const BookCard: React.FC<BookCardProps> = ({
             </Typography>
           )}
         </Stack>
-        <Stack>
+        <Stack style={{height: 250}}>
           <UserListPopup book={book} />
           <Button
             sx={listBooksDeleteButton}
