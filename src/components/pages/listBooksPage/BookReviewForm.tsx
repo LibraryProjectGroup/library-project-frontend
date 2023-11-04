2016c8ab0ed39d7b741cc5e7a94e770eaccd51a0
aarokkeli <aaro.miikkulainen@outlook.com>
2023-11-04 17:55:29 +0200

    Review text max chars. & minimum rating

--- src/components/pages/listBooksPage/BookReviewForm.tsx
+++ src/components/pages/listBooksPage/BookReviewForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Book from '../../../interfaces/book.interface'
 import {
   editBookCancelButton,
@@ -22,13 +22,34 @@ const BookReviewForm: React.FC<BookReviewFormProps> = ({
 }) => {
   const [reviewText, setReviewText] = useState('')
   const [rating, setRating] = useState<number>(0)
+  const [characterCount, setCharacterCount] = useState(500)
+  const [errorText, setErrorText] = useState('')
+
+  useEffect(() => {
+    if (isReviewVisible) {
+      setCharacterCount(500)
+    }
+  }, [isReviewVisible])
+
+  const handleReviewTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newText = e.target.value
+    if (newText.length <= 500) {
+      setReviewText(newText)
+      setCharacterCount(500 - newText.length)
+    }
+  }
 
   const handleReviewSubmit = async () => {
     try {
+      if (rating == 0) {
+        setErrorText('Select a rating before submitting')
+        return
+      }
       const success = await fetchAddReview(book.id, reviewText, rating)
       if (success) {
         setRating(0)
         setReviewText('')
+        setErrorText('')
         setReviewVisible(false)
         loadReviewsAndRating()
       } else {
@@ -49,9 +70,10 @@ const BookReviewForm: React.FC<BookReviewFormProps> = ({
             rows={4}
             fullWidth
             value={reviewText}
-            onChange={(e) => setReviewText(e.target.value)}
+            onChange={handleReviewTextChange}
             margin="normal"
             variant="outlined"
+            helperText={`${characterCount} characters left`}
           />
           <Typography component="legend">Rating:</Typography>
           <Rating
@@ -63,6 +85,11 @@ const BookReviewForm: React.FC<BookReviewFormProps> = ({
               setRating(newValue || 0)
             }}
           />
+          {errorText && (
+            <Typography variant="body2" color="error">
+              {errorText}
+            </Typography>
+          )}
           <Stack direction="row" spacing={3}>
             <Button
               variant="contained"
