47496295adb45b89cf837565182bfa5f0fc34eff
samz9n <samuel.langhoff@outlook.com>
2023-11-07 10:40:25 +0200

    Added option to delete user and tweaked login page to show deletion message

--- src/components/pages/SettingsPage/UserSettings.tsx
+++ src/components/pages/SettingsPage/UserSettings.tsx
@@ -3,6 +3,11 @@ import Paper from '@mui/material/Paper'
 import {
   Box,
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   MenuItem,
   Stack,
   TextField,
@@ -10,8 +15,10 @@ import {
 } from '@mui/material'
 import User from '../../../interfaces/editUser.interface'
 import { HomeOffice } from '../../../interfaces/HomeOffice'
+import { useNavigate } from 'react-router-dom'
 import {
   fetchAllHomeOffices,
+  fetchDeleteUser,
   fetchUpdateUserData,
   fetchUserById,
 } from '../../../fetchFunctions'
@@ -20,13 +27,18 @@ import { confirmButton } from '../../../sxStyles'
 import OfficeSpan from '../../OfficeSpan'
 import { TheContext } from '../../../TheContext'
 import ToastContainers from '../../../ToastContainers'
+import { endSession } from '../../../auth'
 
 const UserSettings: FC = (): JSX.Element => {
+  const navigate = useNavigate()
   const context = useContext(TheContext)
   const [offices, setOffices] = useState<HomeOffice[]>([])
   const [isDataChanged, setIsDataChanged] = useState(false)
   const [userDataFromApi, setUserDataFromApi] = useState<User | null>(null)
   const [userData, setUserData] = useState<User | null>(null)
+  const ErrorMessage = () =>
+    toast.error('Deletion failed', { containerId: 'ToastAlert' })
+  const [open, setOpen] = useState(false)
 
   useEffect(() => {
     // Fetch user data from the API
@@ -52,7 +64,7 @@ const UserSettings: FC = (): JSX.Element => {
       setUserData(updatedUserData)
     }
   }, [userDataFromApi])
-
+  //FETCH ALL OFFICES
   useEffect(() => {
     ;(async () => {
       setOffices(await fetchAllHomeOffices())
@@ -70,6 +82,20 @@ const UserSettings: FC = (): JSX.Element => {
     setIsDataChanged(true)
   }
 
+  const handleOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+  //LOGOUT
+  const handleLogout = () => {
+    endSession()
+    context?.setIsLogin(false)
+  }
+
+  //UPDATE USER
   const updateUser = async (editedUser: User) => {
     const ok = await fetchUpdateUserData(editedUser)
     if (ok?.ok) {
@@ -86,17 +112,31 @@ const UserSettings: FC = (): JSX.Element => {
       console.log('No changes to update')
     }
   }
+  //DELETE USER
+  const deleteUser = async () => {
+    if (!userData) return
+    await fetchDeleteUser(userData?.id).then((res: { ok: any }) => {
+      if (!res.ok) {
+        ErrorMessage()
+      } else {
+        handleLogout()
+        navigate('/login?deletionStatus=success')
+      }
+    })
+    handleClose()
+  }
 
   if (userData == null) return <></>
 
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center' }}>
       <Paper elevation={3} sx={{ marginTop: 7, width: '70%' }}>
-        <Stack spacing={2}>
+        <Stack spacing={2} justifyContent="center" alignItems="center">
           <Typography sx={{ textAlign: 'center' }} variant="h4">
             User Settings
           </Typography>
           <TextField
+            sx={{ width: { xs: '100%', md: '40%' } }}
             disabled
             label="Username"
             name="username"
@@ -104,6 +144,7 @@ const UserSettings: FC = (): JSX.Element => {
             /* onChange={(e) => onChange(e)} */
           />
           <TextField
+            sx={{ width: { xs: '100%', md: '40%' } }}
             disabled
             label="Email"
             name="email"
@@ -111,6 +152,7 @@ const UserSettings: FC = (): JSX.Element => {
             /* onChange={(e) => onChange(e)} */
           />
           <TextField
+            sx={{ width: { xs: '100%', md: '40%' } }}
             select
             label="Set home office"
             name="homeOfficeId"
@@ -126,7 +168,12 @@ const UserSettings: FC = (): JSX.Element => {
               </MenuItem>
             ))}
           </TextField>
-          <Stack direction="row" spacing={2} justifyContent="center">
+          <Stack
+            direction="column"
+            spacing={6}
+            justifyContent="center"
+            alignItems="center"
+          >
             <Button
               disabled={!isDataChanged}
               sx={confirmButton}
@@ -137,6 +184,25 @@ const UserSettings: FC = (): JSX.Element => {
             >
               Update
             </Button>
+            <Button variant="contained" color="error" onClick={handleOpen}>
+              Delete User
+            </Button>
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Confirm Delete</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Are you sure you want to delete this user?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                  Cancel
+                </Button>
+                <Button onClick={deleteUser} color="secondary">
+                  Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
           </Stack>
         </Stack>
         <ToastContainers />
